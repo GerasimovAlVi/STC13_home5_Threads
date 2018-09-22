a303f21eba7deb8078dcb969dd1faf8652c20b47
a12a1 <a12a12a12a@mail.ru>
2018-09-22 10:18:26 +0300

    1

--- src/ru/innopolis/home_work_4/Main.java
+++ src/ru/innopolis/home_work_4/Main.java
@@ -15,7 +15,7 @@ public class Main {
         System.out.println("1:" + System.currentTimeMillis());
         Long startTime2 = System.currentTimeMillis();
         ClassGererateNewTextFile classGererateNewTextFile = new ClassGererateNewTextFile();
-        String[] strWord = {"bbbbbbbbbb", "cccccccccc"};
+        String[] strWord = {"bbbbbbbbbb"};
         String[] strPath = {"d://file1.txt"};
         //String[] strPath = {"d://file1.txt", "d://file2.txt", "d://file3.txt", "d://file4.txt", "d://file5.txt"};
         classGererateNewTextFile.getOccurencies(strPath, strWord, "d://Itog.txt", 5);
