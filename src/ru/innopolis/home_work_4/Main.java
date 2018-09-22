99ac198bbb3600a61a7cc28074a6c65e9e0839fc
a12a1 <a12a12a12a@mail.ru>
2018-09-22 10:01:33 +0300

    1

--- src/ru/innopolis/home_work_4/Main.java
+++ src/ru/innopolis/home_work_4/Main.java
@@ -1,20 +1,23 @@
 package ru.innopolis.home_work_4;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Long startTime1 = System.currentTimeMillis();
+        /*Long startTime1 = System.currentTimeMillis();
         ClassGenerateText classGenerateText = new ClassGenerateText();
         String[] words = new String[]{"aaaaaaaaaa", "bbbbbbbbbb", "cccccccccc"};
-        classGenerateText.getFile("d://", 5, 50000, words, 1);
-        System.out.println("Время записи файлов: " + (System.currentTimeMillis() - startTime1));
+        classGenerateText.getFile("d://", 1, 1500000, words, 1);
+        System.out.println("Время записи файлов: " + (System.currentTimeMillis() - startTime1));*/
 
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
         System.out.println("1:" + System.currentTimeMillis());
         Long startTime2 = System.currentTimeMillis();
         ClassGererateNewTextFile classGererateNewTextFile = new ClassGererateNewTextFile();
-        //String[] strPath = {"d://file1.txt"};
-        //String[] strWord = {"bbbbbbbbbb", "cccccccccc"};
-        String[] strWord = {"bbbbbbbbbb"};
-        String[] strPath = {"d://file1.txt", "d://file2.txt", "d://file3.txt", "d://file4.txt", "d://file5.txt"};
+        String[] strWord = {"bbbbbbbbbb", "cccccccccc"};
+        String[] strPath = {"d://file1.txt"};
+        //String[] strPath = {"d://file1.txt", "d://file2.txt", "d://file3.txt", "d://file4.txt", "d://file5.txt"};
         classGererateNewTextFile.getOccurencies(strPath, strWord, "d://Itog.txt", 5);
         System.out.println("Время анализа файлов и записи в один файл: " + (System.currentTimeMillis() - startTime2));
     }
